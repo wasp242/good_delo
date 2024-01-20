502228552988ae42e01fbfd7856b513e3decef86
Aleksandr Dosaev <alexander.dosaev@gmail.com>
2024-01-20 20:24:07 +0400

    fixed arrow hover

--- src/components/leftside.js
+++ src/components/leftside.js
@@ -32,22 +32,7 @@ export const Leftside = () => {
           <p className="">дней без душноты 0</p>
           <button className="btn__text">
             <span>История</span>
-            <svg
-              className="svg"
-              xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="29"
-              viewBox="0 0 28 29"
-              fill="none"
-            >
-              <path
-                d="M24.5 14.2521L18.6667 8.41879M24.5 14.2521L18.6667 20.0855M24.5 14.2521H3.5"
-                stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <span className="icon arrow"></span>
           </button>
         </div>
         <div className="leftside-bottom__logo">
